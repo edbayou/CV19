142ef685f0769866704770cb1fd0f99c52059605
Эдуард Баянов <1550hart@gmail.com>
2021-02-28 23:57:55 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Test/CV19Console/Program.cs
+++ Test/CV19Console/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV19Console
 {
     class Program
     {
+        //источник данных
+        const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //старая версия WebClient
+            var client = new HttpClient();
+            //получаем ответ от сервера
+            var response = client.GetAsync(data_url).Result;//в конце пишем результ чтбы не делать весь метод асинхронным
+            //читаем данные
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+            Console.ReadLine();
         }
     }
 }
