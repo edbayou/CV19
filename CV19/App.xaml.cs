2193a47eb977cc92c37eea44cbf4aab5480d5801
Эдуард Баянов <1550hart@gmail.com>
2021-03-06 16:34:58 +0300

    Чистка хлама

--- CV19/App.xaml.cs
+++ CV19/App.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace CV19
+﻿namespace CV19
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
