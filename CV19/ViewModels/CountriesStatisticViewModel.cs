f7e7a689333286433182a1ec941d9d47b43ae6f9
Эдуард Баянов <1550hart@gmail.com>
2021-03-12 22:13:51 +0300

    Базовое представление модели представления статистики

--- CV19/ViewModels/CountriesStatisticViewModel.cs
+++ CV19/ViewModels/CountriesStatisticViewModel.cs
@@ -1,15 +1,49 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.Services;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
     internal class CountriesStatisticViewModel : ViewModel
     {
-        private readonly MainWindowViewModel _MainModel;
+        //сервис который будет извлекать данные
+        private DataService _DataService;
+        //можно так 
+        //private readonly MainWindowViewModel _MainModel;
+        //но мы сделаем как свойство чтобы можно было достучаться из представления
+        private MainWindowViewModel MainModel { get; }
+
+        #region Свойство Contries : IEnumerable<CountryInfo> - Будет содержать статистику по странам и при этом seter будет приватным 
+        ///<summary> Будет содержать статистику по странам и при этом seter будет приватным </summary>
+        private IEnumerable<CountryInfo> _Contries;
+
+        public IEnumerable<CountryInfo> Contries
+        {
+            get => _Contries;
+            private set => Set(ref _Contries, value); }
+        #endregion
+
+        //прописываем команды 
+        #region Команды.
+        public ICommand RefreshDataCommand { get; }
+        //будет выгружать данные из сервиса
+        private void OnRefreshDataCommandExecuted(object p) 
+        {
+            Contries = _DataService.GetData();
+        }
+        #endregion
         //связваем вюмодели между собой при этом в главной создаем вторичную
-        public CountriesStatisticViewModel(MainWindowViewModel MainModel) { _MainModel = MainModel; }
+        public CountriesStatisticViewModel(MainWindowViewModel MainModel) 
+        { 
+           this.MainModel = MainModel;
+            _DataService = new DataService();
+            RefreshDataCommand = new LambdaCommand(OnRefreshDataCommandExecuted);
+        }
         //теперь мы сможем вызывать методы из основной модели а ана у нас и также передавать/устанавливать значения свойст
     }
 }
