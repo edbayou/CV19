80dce2fa167f9b334ce1e2715746a32714483967
Эдуард Баянов <1550hart@gmail.com>
2021-03-17 22:12:52 +0300

    Мультиконвертер

---
+++ CV19/Infrastructure/Converters/MultiConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CV19.Infrastructure.Converters
+{
+    internal abstract class MultiConverter : IMultiValueConverter
+    {
+        //принимаем много бъектов и один тип
+        public abstract object Convert(object[] vv, Type t, object p, CultureInfo c);
+        // принимаем один объект и множество типов
+        public virtual object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) =>
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+
+    }
+}
---
+++ CV19/Infrastructure/Converters/ToArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+namespace CV19.Infrastructure.Converters
+{
+    class ToArray : MultiConverter
+    {
+        //возвращаем значение к которому привязваемся
+        public override object Convert(object[] vv, Type t, object p, CultureInfo c)
+        {
+            //помогает отобразить несколько коллекций и элементов в виде одного списка - CompositeCollection
+            var collection = new CompositeCollection();
+            foreach (var value in vv)
+                collection.Add(value);
+            return collection;
+        }
+        //возвращаем значение в виде массива, пока не поддерживаем
+        //public override object[] ConvertBack(object v, Type[] tt, object p, CultureInfo c) => v as object[];
+    }
+}
